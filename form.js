7e7ec63e26e4ecb0f50ba2660058d2f76efe9222
Gerardo I Contreras <gcontreras@r2hstudent.org>
2022-07-02 17:44:24 -0500

    editted files

---
+++ form.js
@@ -0,0 +1,29 @@
+const name = document.getElementById('name')
+const phone = document.getElementById('phone')
+const email = document.getElementById('email')
+const form = docuement.getElementById('form')
+const submit = document.getElementById('submit')
+const errorElement = document.getElementById('error')
+
+submit.preventDefault()
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+    if (phone.value === phone.value || phone.value == null){
+        messages.push('Name is required')
+    }
+    if (email.value.length <= 10) {
+        email.push('Email must be longer than 10 characters')
+    }
+    if (form.value === '' || form.value == null){
+        messages.push('Name is required')
+    }
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+    
+})
---
+++ scroll.js
@@ -0,0 +1,7 @@
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    console.log
+
+}
+
+smoothScroll('.section2', 1000)
